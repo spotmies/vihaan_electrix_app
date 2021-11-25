643dedfc71d5266127c13af7e8cb12b4e2ad4e4d
satishkumarsaride <satishkumarsaride192@gmail.com>
2021-11-25 15:09:21 +0530

    user registration

--- vihaanelectrix/lib/views/login/user_registration.dart
+++ vihaanelectrix/lib/views/login/user_registration.dart
@@ -1,27 +1,34 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:vihaanelectrix/controllers/login.dart/user_registration_controller.dart';
 import 'package:vihaanelectrix/widgets/app_config.dart';
 
 class UserRegistration extends StatefulWidget {
-  const UserRegistration({ Key? key }) : super(key: key);
+  const UserRegistration({Key? key}) : super(key: key);
 
   @override
   _UserRegistrationState createState() => _UserRegistrationState();
 }
 
 class _UserRegistrationState extends State<UserRegistration> {
-  UserRegistrationController userRegistrationController = UserRegistrationController();
+  UserRegistrationController userRegistrationController =
+      UserRegistrationController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: userRegistration(context, userRegistrationController),
+      body: Column(
+        children: [
+          profilePic(context, userRegistrationController),
+          userRegistration(context, userRegistrationController),
+        ],
+      ),
     );
   }
 }
 
-
-
-Widget userRegistration(BuildContext context, UserRegistrationController userRegistrationController) {
+Widget userRegistration(BuildContext context,
+    UserRegistrationController userRegistrationController) {
   return Column(
     children: [
       SizedBox(
@@ -34,8 +41,7 @@ Widget userRegistration(BuildContext context, UserRegistrationController userReg
               decoration: BoxDecoration(
                   color: Colors.white, borderRadius: BorderRadius.circular(15)),
               child: TextFormField(
-                onSaved: (item) =>
-                    userRegistrationController.name,
+                onSaved: (item) => userRegistrationController.name,
                 keyboardType: TextInputType.name,
                 controller: userRegistrationController.nameTf,
                 decoration: InputDecoration(
@@ -65,90 +71,83 @@ Widget userRegistration(BuildContext context, UserRegistrationController userReg
                 },
               ),
             ),
-            SizedBox(
-              height: 7,
-            ),
-            Container(
-              //padding: EdgeInsets.all(10),
-              height: height(context) * 0.1,
-              width: width(context) * 1,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(15)),
-              child: TextFormField(
-                onSaved: (item) =>
-                    userRegistrationController.email,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                      borderSide: BorderSide(width: 1, color: Colors.white)),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                      borderSide: BorderSide(width: 1, color: Colors.white)),
-                  hintStyle: TextStyle(fontSize: 17, color: Colors.grey),
-                  hintText: 'Email(Optional)',
-                  suffixIcon: Icon(Icons.email),
-                  //border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(20),
-                ),
-                validator: (value) {
-                  if (value!.length > 1 && !value.contains('@')) {
-                    return 'Please Enter Valid Email';
-                  }
-                  return null;
-                },
-                controller: userRegistrationController.emailTf,
-                onChanged: (value) {
-                  userRegistrationController.email = value;
-                },
-              ),
-            ),
-            SizedBox(
-              height: 7,
-            ),
-            Container(
-              //padding: EdgeInsets.all(10),
-              height: height(context) * 0.1,
-              width: width(context) * 1,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(15)),
-              child: TextFormField(
-                maxLength: 10,
-                onSaved: (item) =>
-                    userRegistrationController.altnumber,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  // labelText: "optional",
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                      borderSide: BorderSide(width: 1, color: Colors.white)),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                      borderSide: BorderSide(width: 1, color: Colors.white)),
-                  hintStyle: TextStyle(fontSize: 17, color: Colors.grey),
-                  hintText: 'Alternative Mobile (optional)',
-                  suffixIcon: Icon(Icons.dialpad),
-                  //border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(20),
-                  counterText: "",
-                ),
-                validator: (value) {
-                  if (value!.length == 10 && int.parse(value) < 5000000000) {
-                    return 'Please Enter Valid Mobile Number';
-                  } else if (value.isNotEmpty && value.length < 10) {
-                    return 'Please Enter Valid Mobile Number';
-                  }
-                  return null;
-                },
-                controller: userRegistrationController.altnumberTf,
-                onChanged: (value) {
-                  userRegistrationController.altnumber = value;
-                },
-              ),
-            ),
           ],
         ),
       ),
+    ],
+  );
+}
+
+Widget profilePic(BuildContext context,
+    UserRegistrationController userRegistrationController) {
+  return Column(
+    children: [
+      SizedBox(
+        height: height(context) * 0.375,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+         
+          Center(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              height: height(context) * 0.15,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)),
+              ),
+              child: CircleAvatar(
+                child: Center(
+                  child: userRegistrationController.profilepic == null
+                      ? Icon(
+                          Icons.person,
+                          color: Colors.blueGrey,
+                          size: width(context) * 0.15,
+                        )
+                      : Container(
+                          height: height(context) * 0.27,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                          child:
+                              CircleAvatar(
+                            backgroundImage: FileImage(
+                                userRegistrationController.profilepic!),
+                            radius: 100,
+                          ),
+                        ),
+                ),
+                radius: 30,
+                backgroundColor: Colors.grey[100],
+              ),
+            ),
+          ),
+          Container(
+            height: 40,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30)),
+              // border: Border.all()
+            ),
+            child: TextButton(
+                onPressed: () async {
+                  await userRegistrationController.profilePic();
+                  userRegistrationController.refresh();
+
+                  log(userRegistrationController.profilepic.toString());
+                },
+                // icon: Icon(Icons.select_all),
+                child: Text(
+                  'Choose Image',
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                )),
+          ),
+        ]),
+      )
     ],
   );
 }
